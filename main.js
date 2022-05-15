8e5faa80fca2af9e01053cb98282495391e27acb
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-05-15 15:00:39 +0100

    Updated Basic Markup

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 // COLOUR SELECTOR
 
 const pickColourBtns = document.getElementsByClassName("button")
-//console.log(pickColourBtn)
+const generateBtnMobile = document.getElementById("generate")
 const backgroundColour = document.getElementById("colour")
-const newHexCode = document.getElementById("hex_colour")
-
 const backgroundColour_1 = document.getElementById("colour_1")
 const backgroundColour_2 = document.getElementById("colour_2")
 
-//pickColourBtn.addEventListener("click", getNewColour)
+const newHexCode = document.getElementById("hex_colour")
+const newHexCode_1 = document.getElementById("hex_colour_1")
+const newHexCode_2 = document.getElementById("hex_colour_2")
 
-// Click Eventlistener for each button
+// Click Eventlistener For Each Colour
 for (let i = 0; i < pickColourBtns.length; i++) {
   pickColourBtns[i].addEventListener("click", getNewColour)
-  console.log(pickColourBtns[i])
 }
+
+// Click Eventlistener For Spacebar
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    getNewColour()
+  }
+})
+
+// Click Eventlistener For Mobile Layout
+generateBtnMobile.addEventListener("click", getNewColour)
 
 //Generate New Colour Function
 function getNewColour() {
@@ -33,4 +42,6 @@ function getNewColour() {
   backgroundColour_1.style.background = colour_1
   backgroundColour_2.style.background = colour_2
   newHexCode.innerText = colour
+  newHexCode_1.innerText = colour_1
+  newHexCode_2.innerText = colour_2
 }
